46da92857d0cd637f31b35e3ec1ab8db831aa9ba
riddhi kumari <riddhilal14vdb@gmail.com>
2021-07-19 17:44:41 +0530

    commit

--- src/app/home/kyc/kyc.page.ts
+++ src/app/home/kyc/kyc.page.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kyc.page.scss'],
 })
 export class KycPage implements OnInit {
+  otpController(event,next,prev){
+    if(event.target.value.length < 1 && prev){
+      prev.setFocus()
+    }
+    else if(next && event.target.value.length>0){
+      next.setFocus();
+    }
+    else {
+     return 0;
+    } 
+  }
 
-  constructor() { }
+  constructor() {
+  }
+   
 
   ngOnInit() {
   }
 
 }
+
